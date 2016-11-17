059e905b5205588e3dafdb5bbf9876848749b874
fzhao92 <fozhao92@gmail.com>
2016-11-17 13:22:46 -0500

    add firebase contact ref, add auth state change listener, grab logged in user email and uid from firebase

--- PotatoParty/PotatoParty/ContactsViewController.swift
+++ PotatoParty/PotatoParty/ContactsViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class ContactsViewController: UIViewController {
 
+    let ref = FIRDatabase.database().reference(withPath: "contacts")
+    var user: User?
+    var userUid: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.restorationIdentifier = "contactsVC"
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.isHidden = true
+        
+        FIRAuth.auth()?.addStateDidChangeListener({ (auth, user) in
+            guard let user = user else { return }
+            self.user = User(authData: user)
+            self.userUid = user.uid
+            print("Current logged in user email - \(self.user?.email)")
+            
+        })
+        
+        ref.observe(.value, with: { (snapshot) in
+            print("new updates from firebase")
+        })
     }
 
     override func didReceiveMemoryWarning() {
