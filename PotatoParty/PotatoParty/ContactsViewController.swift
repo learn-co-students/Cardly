9ffe60b95ab50a98b030d0be32f7dc5c84a29f07
MichelleBKlynn <michellestaton@gmail.com>
2016-11-17 19:33:48 -0500

    Added dropdown menu to navbar

--- PotatoParty/PotatoParty/ContactsViewController.swift
+++ PotatoParty/PotatoParty/ContactsViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 import FirebaseDatabase
 import FirebaseAuth
+import SnapKit
 
-class ContactsViewController: UIViewController {
 
+class ContactsViewController: UIViewController, DropDownMenuDelegate {
+    public func didTapInDropDownMenuBackground(_ menu: DropDownMenu) {
+        
+    }
+
+    var navigationBarMenu: DropDownMenu!
+    var titleView: DropDownTitleView!
     let ref = FIRDatabase.database().reference(withPath: "contacts")
     var user: User?
     var userUid: String?
@@ -22,17 +29,18 @@ class ContactsViewController: UIViewController {
         self.restorationIdentifier = "contactsVC"
         // Do any additional setup after loading the view.
         
-        var dropDownMenu = DropDownMenu(
-        
-        
+        self.navigationBarMenu = DropDownMenu()
+
         
         self.view.backgroundColor = UIColor.red
         
-        
+        let title = prepareNavigationBarMenuTitleView()
+        prepareNavigationBarMenu(title)
         
         let rightBtn = UIBarButtonItem(title: "Hey", style: .plain, target: self, action: nil)
         self.navigationItem.rightBarButtonItem = rightBtn
-        
+        // Write some code in place of "nil" after "action:"
+        // Make left button and
         
         
         
@@ -53,6 +61,94 @@ class ContactsViewController: UIViewController {
             }
         })
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    
+        navigationBarMenu.container = view
+        
+        
+        //toolbarMenu.container = view
+    }
+    
+    
+
+    func prepareNavigationBarMenuTitleView() -> String {
+        // Both title label and image view are fixed horizontally inside title
+        // view, UIKit is responsible to center title view in the navigation bar.
+        // We want to ensure the space between title and image remains constant,
+        // even when title view is moved to remain centered (but never resized).
+        titleView = DropDownTitleView(frame: CGRect(x: 0, y: 0, width: 150, height: 40))
+        
+        
+        
+        titleView.addTarget(self, action: Selector("willToggle"), for: .touchUpInside)
+        
+        titleView.titleLabel.textColor = UIColor.black
+        titleView.title = "Lists"
+        
+        navigationItem.titleView = titleView
+        
+        return titleView.title!
+    }
+    
+    
+    func hey(){
+        
+    }
+    
+    func willToggle(){
+        if self.titleView.isUp{
+            navigationBarMenu.hide()
+        }else{
+            navigationBarMenu.show()
+        }
+       
+        
+    }
+    
+  
+
+    
+    func prepareNavigationBarMenu(_ currentChoice: String) {
+        navigationBarMenu = DropDownMenu(frame: view.bounds)
+        
+        navigationBarMenu.delegate = self
+        
+        let firstCell = DropDownMenuCell()
+        
+        firstCell.textLabel!.text = "List 1"
+        firstCell.menuAction = nil
+        firstCell.menuTarget = self
+        if currentChoice == "List 1" {
+            firstCell.accessoryType = .checkmark
+        }
+        
+        let secondCell = DropDownMenuCell()
+        
+        secondCell.textLabel!.text = "List 2"
+        secondCell.menuAction = nil
+        secondCell.menuTarget = self
+        if currentChoice == "List 2" {
+            firstCell.accessoryType = .checkmark
+        }
+        
+        navigationBarMenu.menuCells = [firstCell, secondCell]
+        navigationBarMenu.selectMenuCell(secondCell)
+        
+        // If we set the container to the controller view, the value must be set
+        // on the hidden content offset (not the visible one)
+        navigationBarMenu.visibleContentOffset =
+            navigationController!.navigationBar.frame.size.height + 24
+        
+        // For a simple gray overlay in background
+        navigationBarMenu.backgroundView = UIView(frame: navigationBarMenu.bounds)
+        navigationBarMenu.backgroundView!.backgroundColor = UIColor.black
+        navigationBarMenu.backgroundAlpha = 0.7
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
