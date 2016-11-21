16e7203d932e4de8702d88b895c47e0f2f8bca6d
daveneff <beefylogs@gmail.com>
2016-11-21 10:21:04 -0500

    Added Constants.swift

---
+++ PotatoParty/PotatoParty/Constants.swift
@@ -0,0 +1,32 @@
+//
+//  Constants.swift
+//  PotatoParty
+//
+//  Created by Dave Neff on 11/21/16.
+//  Copyright © 2016 ForrestApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Styles {
+    static let cornerRadius: CGFloat = 7
+}
+
+struct Font {
+    static let name = "Helvetica Neue UltraLight"
+    
+    struct Size {
+        static let xs: CGFloat = 8
+        static let s: CGFloat = 12
+        static let m: CGFloat = 18
+        static let l: CGFloat = 24
+        static let xl: CGFloat = 36
+        static let xxl: CGFloat = 48
+    }
+}
+
+struct Colors {
+    static let orange = UIColor(red: 225.0/255.0, green: 50.0/255.0, blue: 29.0/255.0, alpha: 1)
+    
+}
