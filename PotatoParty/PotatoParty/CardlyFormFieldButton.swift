564fca13a0d5e103733de1a7cc31fc566733f4be
fzhao92 <fozhao92@gmail.com>
2016-12-10 21:06:56 -0500

    add highlight for button, hacky but works

--- PotatoParty/PotatoParty/CardlyFormFieldButton.swift
+++ PotatoParty/PotatoParty/CardlyFormFieldButton.swift
@@ -13,12 +13,24 @@ struct CardlyFormFieldButton {
     
     static func initButton(title: String, target: Any?, selector: Selector) -> UIButton {
         let button = UIButton()
-        button.setTitle(title, for: .normal)
-        button.titleLabel!.font = UIFont(name: Font.regular, size: Font.Size.l)
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 35)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(UIColor.gray.cgColor)
+        context?.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        button.setTitle("  \(title)  ", for: .normal)
+        button.titleLabel!.font = UIFont(name: Font.regular, size: Font.Size.m)
         button.setTitleColor(UIColor.gray, for: .normal)
         button.setTitleColor(Colors.cardlyGold, for: .highlighted)
+        button.setBackgroundImage(image, for: .highlighted)
         button.titleLabel!.minimumScaleFactor = 0.5
         button.addTarget(target, action: selector, for: .touchUpInside)
+        button.layer.cornerRadius = Styles.cornerRadius
+        button.layer.borderWidth = 1.5
+        button.layer.masksToBounds = true
+        button.layer.borderColor = UIColor.gray.cgColor
         button.sizeToFit()
         return button
     }
